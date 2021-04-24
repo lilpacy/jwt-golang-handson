57f5eeb0add7f3765ef69706ec4708f3ddefbf20
LilPacy <revivedtomorrow@gmail.com>
2021-04-25 00:42:57 +0900

    change to web api

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-    "os"
+    "encoding/json"
     "log"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
-func main() {
-    http.Handle("/", http.FileServer(http.Dir("views")))
-    log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), nil))
+type post struct {
+    Title string `json:"title"`
+    Tag   string `json:"tag"`
+    URL   string `json:"url"`
 }
+
+func main() {
+    r := mux.NewRouter()
+    // localhost:8080/publicでpublicハンドラーを実行
+    r.Handle("/public", public)
+
+    //サーバー起動
+    if err := http.ListenAndServe(":8080", r); err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    post := &post{
+        Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
+        Tag:   "Vue.js",
+        URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+    }
+    json.NewEncoder(w).Encode(post)
+})
